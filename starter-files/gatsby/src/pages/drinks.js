ece1ff47e222cce312295632c696e7dfed0956b0
Sadia Ansari <sadia765@gmail.com>
2020-09-25 13:20:28 -0500

    added pages and navigation to pizza site

---
+++ starter-files/gatsby/src/pages/drinks.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function DrinksPage() {
+  return (
+    <div>
+      <p> Hey! I'm the Drinks page</p>
+    </div>
+  );
+}
---
+++ starter-files/gatsby/src/pages/order.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default function OrderPage() {
+  return (
+    <div>
+      <p> Hey! I'm the Order page</p>
+    </div>
+  );
+}
